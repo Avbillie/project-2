29cd1d4e2ff080f90def50c2ae2db5a05f7a3906
Anthony_Billie_Jr <67110957+Avbillie@users.noreply.github.com>
2020-11-14 14:40:19 -0500

    added schema and seed sql files

---
+++ seed.sql
@@ -0,0 +1,4 @@
+INSERT INTO Users (email,password,createdAt,updatedAt) VALUES ('abillie994@gmail.com','1234',now(),now());
+INSERT INTO Users (email,password,createdAt,updatedAt) VALUES ('dina.m.1826@gmail.com','12345',now(),now());
+INSERT INTO Users (email,password,createdAt,updatedAt) VALUES ('sillahsankung@gmail.com','123456',now(),now());
+INSERT INTO Users (email,password,createdAt,updatedAt) VALUES ('ducthesmallblue@yahoo.com','1234567',now(),now());
