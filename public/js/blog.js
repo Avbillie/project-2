b14b1991b136a799778e73baad139a05d65c66d9
Sankung Sillah <ssillah306@gmail.com>
2020-11-15 23:16:20 -0500

    added the code needed to send the data to the backend

--- public/js/blog.js
+++ public/js/blog.js
@@ -1,10 +1,42 @@
-$(document).ready(()=>{
+$(document).ready(() => {
 
-const submit = $("submit");
-const userName = $("userName");
-const title = $("title");
-const text = $("text");
+    const submit = $("submit");
+    const userName = $("userName");
+    const title = $("title");
+    const text = $("text");
 
+    submit.on("submit", event => {
+        event.preventDefault();
 
+        const inputData = {
+            userName = userName.val().trim(),
+            title = title.val(),
+            text = text.val()
+        }
 
+        // making the sure enters the user name, title and the blog text before they can submit
+
+        if (!inputData.userName && !inputData.title && !inputData.text) {
+
+            return;
+        } else {
+            blogData(inputData.userName, inputData.title, inputData.text);
+            userName.val("");
+            title.val("");
+            text.val("");
+        }
+    });
+
+    function blogData(userName, title, text) {
+        $post("api/blog", {
+            userName: userName,
+            title: title,
+            text: text
+        }).then(() => {
+            window.location.replace("/members");
+        }).catch(err => {
+            console.log(err);
+        });
+
+    }
 });
