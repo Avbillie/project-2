2195d610c80b802036b11477210f26c902ac2ae7
Sankung Sillah <ssillah306@gmail.com>
2020-11-16 19:02:03 -0500

    fixed the issue with the travis ci

--- public/js/blog.js
+++ public/js/blog.js
@@ -1,42 +1,40 @@
 $(document).ready(() => {
+  const submit = $("#submit");
+  const userName = $("#userName");
+  const title = $("#title");
+  const text = $("#text");
 
-    const submit = $("#submit");
-    const userName = $("#userName");
-    const title = $("#title");
-    const text = $("#text");
+  submit.on("submit", event => {
+    event.preventDefault();
 
-    submit.on("submit", event => {
-        event.preventDefault();
+    const inputData = {
+      userName: userName.val().trim(),
+      title: title.val(),
+      text: text.val()
+    };
 
-        const inputData = {
-            userName = userName.val().trim(),
-            title = title.val(),
-            text = text.val()
-        }
+    // making the sure enters the user name, title and the blog text before they can submit
 
-        // making the sure enters the user name, title and the blog text before they can submit
-
-        if (!inputData.userName || !inputData.title || !inputData.text) {
-
-            return;
-        } else {
-            blogData(inputData.userName, inputData.title, inputData.text);
-            userName.val("");
-            title.val("");
-            text.val("");
-        }
-    });
-
-    function blogData(userName, title, text) {
-        $post("api/blog", {
-            userName: userName,
-            title: title,
-            text: text
-        }).then(() => {
-            window.location.replace("/members");
-        }).catch(err => {
-            console.log(err);
-        });
-
+    if (!inputData.userName || !inputData.title || !inputData.text) {
+      return;
     }
+    blogData(inputData.userName, inputData.title, inputData.text);
+    userName.val("");
+    title.val("");
+    text.val("");
+  });
+
+  function blogData(name, blogTitle, blogText) {
+    $post("api/blog", {
+      userName: name,
+      title: blogTitle,
+      text: blogText
+    })
+      .then(() => {
+        window.location.replace("/members");
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
 });
