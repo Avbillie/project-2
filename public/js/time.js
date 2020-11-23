796a8401daf0f763b3b1e6a6c7387a0d0bc5f540
Sankung Sillah <ssillah306@gmail.com>
2020-11-22 20:44:05 -0500

    update

---
+++ public/js/time.js
@@ -0,0 +1,11 @@
+const timeData = $(".timeData");
+
+const timeVal = timeData.text();
+console.log(timeVal);
+
+const convertedTime = moment(timeVal).format("dddd, MMMM Do YYYY");
+console.log(convertedTime);
+
+timeData.val("");
+
+timeData.text(convertedTime);
